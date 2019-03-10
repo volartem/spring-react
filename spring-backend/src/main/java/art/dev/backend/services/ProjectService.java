8a71b5a5154e4771d2e4c29f2db8e83f991d3923
Artem <artem.volshynskyi@gmail.com>
2019-03-10 17:11:37 +0200

    Add exceptions, controller advice exceptions, project identifier exception.

--- spring-backend/src/main/java/art/dev/backend/services/ProjectService.java
+++ spring-backend/src/main/java/art/dev/backend/services/ProjectService.java
@@ -1,6 +1,7 @@
 package art.dev.backend.services;
 
 import art.dev.backend.domains.Project;
+import art.dev.backend.exceptions.ProjectIdException;
 import art.dev.backend.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,13 @@ public class ProjectService {
 
     public Project saveOrUpdate(Project project) {
 
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project with projectIdentifier " + project.getProjectIdentifier().toUpperCase()
+                    + " already exists!");
+        }
     }
 
 }
